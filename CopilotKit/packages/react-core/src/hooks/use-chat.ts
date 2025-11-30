130979a29e8acca7077c02c1da4580f0c1a954df
Stanislav Malyshevskyi <cetecnine@gmail.com>
2025-11-30 23:27:14 +0100

    fix guard prop

--- CopilotKit/packages/react-core/src/hooks/use-chat.ts
+++ CopilotKit/packages/react-core/src/hooks/use-chat.ts
@@ -400,7 +400,7 @@ export function useChat(options: UseChatOptions): UseChatHelpers {
                             },
                           },
                         }
-                      : {}),
+                      : null),
                   },
                 }
               : {}),
